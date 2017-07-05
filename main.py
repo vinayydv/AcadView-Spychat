f73ca0cff7971b389e019a6ec1701516c7e0d332
Vinay yadav <404.vinay@gmail.com>
2017-07-05 17:03:55 +0530

    Getting details of the spy

---
+++ main.py
@@ -0,0 +1,15 @@
+print "Welcome to Spy Chat"
+
+#details about the spy
+spy_name = raw_input("What do you want to be called as a spy?")
+
+print "Hello " + spy_name
+
+spy_salutation = raw_input("Are you Mr or Miss?")
+spy_name = spy_salutation + spy_name #concatinating name and salutation
+
+print "What is your age "+ spy_name
+
+spy_age = raw_input("What is your age ?")
+
+print  "Ok! " +spy_name+ " aged " +spy_age+ ". Good to have you here :) ."
